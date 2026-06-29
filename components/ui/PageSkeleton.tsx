b62882311ca57d0b16b40967a3e167d9b10b8ce7
BU3NO77XL <danielbuenodev01@gmail.com>
2026-06-29 01:04:47 +0000

    Corrige dimensões dos skeletons da home para espelhar UI real
    - Hero: altura fixa 810px (igual HeroSection), título mais alto (h-16/20), sinopse com 3 linhas (line-clamp-3), botões com md:h-52px
    - Cards do carrossel: aspect-[2/3] (era aspect-video), largura 140/150/160px (era 160/190px)
    - Padding do carrossel: pl-4 sm:pl-6 lg:pl-[24px] (era px-4 md:px-[38px])
    - Gap: lg:gap-4 adicionado

--- components/ui/PageSkeleton.tsx
+++ components/ui/PageSkeleton.tsx
@@ -6,14 +6,18 @@ export default function PageSkeleton() {
     return (
         <div className="min-h-screen bg-[#121212]">
             {/* Hero Skeleton */}
-            <div className="relative w-full h-[85vh] md:h-[80vh] bg-[#1a1a1a] overflow-hidden">
+            <div className="relative w-full h-[810px] bg-[#1a1a1a] overflow-hidden">
                 <div className="absolute inset-0 bg-gradient-to-t from-[#121212] via-transparent to-transparent" />
                 <div className="absolute bottom-[15%] left-6 md:left-12 max-w-xl space-y-5">
-                    <div className={`h-10 md:h-14 w-72 md:w-96 rounded-lg ${shimmer}`} />
-                    <div className={`h-4 w-48 md:w-64 rounded ${shimmer}`} />
+                    <div className={`h-16 md:h-20 w-72 md:w-96 rounded-lg ${shimmer}`} />
+                    <div className="space-y-2">
+                        <div className={`h-4 w-full max-w-[518px] rounded ${shimmer}`} />
+                        <div className={`h-4 w-3/4 max-w-[400px] rounded ${shimmer}`} />
+                        <div className={`h-4 w-1/2 max-w-[300px] rounded ${shimmer}`} />
+                    </div>
                     <div className="flex gap-3 pt-2">
-                        <div className={`h-[50px] w-[140px] rounded-[4px] ${shimmer}`} />
-                        <div className={`h-[50px] w-[190px] rounded-[4px] ${shimmer}`} />
+                        <div className={`h-[50px] md:h-[52px] w-[130px] md:w-[150px] rounded-[4px] ${shimmer}`} />
+                        <div className={`h-[50px] md:h-[52px] w-[170px] md:w-[190px] rounded-[4px] ${shimmer}`} />
                     </div>
                 </div>
             </div>
@@ -21,13 +25,13 @@ export default function PageSkeleton() {
             {/* Carousels Skeleton */}
             <div className="relative z-20 -mt-[211px] pb-12 space-y-8">
                 {[1, 2, 3, 4, 5].map((section) => (
-                    <div key={section} className="space-y-3 px-4 md:px-[38px]">
-                        <div className={`h-5 w-44 rounded ${shimmer}`} />
-                        <div className="flex gap-3 overflow-hidden">
+                    <div key={section} className="space-y-3 pl-4 sm:pl-6 lg:pl-[24px] pr-0">
+                        <div className={`h-6 w-44 rounded ${shimmer}`} />
+                        <div className="flex gap-3 lg:gap-4 overflow-hidden">
                             {[1, 2, 3, 4, 5, 6, 7].map((card) => (
                                 <div
                                     key={card}
-                                    className={`shrink-0 w-[160px] md:w-[190px] aspect-video rounded-[4px] ${shimmer}`}
+                                    className={`shrink-0 w-[140px] sm:w-[150px] lg:w-[160px] aspect-[2/3] rounded-[4px] ${shimmer}`}
                                 />
                             ))}
                         </div>
